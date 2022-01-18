b513574dd7191734b69299c0da5ad0b42b70e7c9
Usha <ushanaks@gmail.com>
2022-01-18 14:41:43 -0500

    latest changes done to HDFC-300 by dev1

--- sample-file.java
+++ sample-file.java
@@ -45,13 +45,23 @@ public class Request {
 			
 			
 			public class DemoClass{
- // new code for HDFC-300 userstory is added by Dev1
+ //latest code is added by dev1//// new code for HDFC-300 userstory is added by Dev1
+ package com.softwaretestinghelp;
  
- public static void main (String[] args){
-    System.out.println("Hello , this is sample program");   
-    double d = Math.random();
-    System.out.println("Random Number:"+d);
+public class DemoClass{
+public static int  sampleMethod(int a,int b) throws ArithmeticException{ 
+//[1] int as data type of return value
+      System.out.println("Hello, this is sample method");
+      int c = a/b; // int c = 4 / 2 gets calculated as 2 
+      System.out.println("c:"+c); // c: 2
+      return c;//return statement 
+ } 
+ 
+public static void main (String[] args){
+    int x = DemoClass.sampleMethod(4,2);//return value is assigned to variable x 
+    System.out.println("x:"+x); // x: 2
  }
+}
         }
     }
 }
